0bb9994d28f14e99dcf298382cb49ec200e4fecb
AlexCadigan <cadiganalexander@gmail.com>
2018-02-25 02:03:43 -0500

    Animations are working for all objects

--- src/MutualExclusionDemonstration.java
+++ src/MutualExclusionDemonstration.java
@@ -1,310 +1,83 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Timestamp;
-import javafx.animation.Animation;
-import javafx.animation.PathTransition;
+/*
+MIT License
+
+Copyright (c) 2018 Alex Cadigan, Jacob Naranjo, Tanush Samson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.Group;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.Scene;
-import javafx.scene.shape.ArcTo;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.HLineTo;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.VLineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.QuadCurveTo;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javax.swing.JOptionPane;
-import javafx.util.Duration;
-
-/*
-* Contains code to construct a GUI that allows users to initiate the simulation
-*/
-public class MutualExclusionDemonstration extends Application
-{
-	/*
-	* Main method that begins the creation of the GUI
-	*/
-	public static void main(String [] args)
-	{
+/* Runs a simulation that demonstrates mutual exclusion in distributed systems */
+public class MutualExclusionDemonstration extends Application {
+	/* Begins the simulation */
+	public static void main(String [] args) {
 		Application.launch(args);
 	}
-
-	/*
-	* Creates GUI page for users to enter ball speeds
-	*/
+	/* Creates a GUI window for users to enter simulation settings */
 	@Override
-	public void start(Stage stage) 
-	{
+	public void start(Stage stage) {
 		stage.setTitle("Mutual Exclusion Demonstration");
-
 		// Object speed labels
+		Label lblInstructions = new Label("Note: objects move faster with lower speeds!");
+		HBox label0 = new HBox(10);
+		label0.setAlignment(Pos.CENTER);
+		label0.getChildren().addAll(lblInstructions);
 		Label lblCircle = new Label("Circle Speed:");
 		TextField txtCircle = new TextField("10");
 		HBox label1 = new HBox(10);
 		label1.setAlignment(Pos.CENTER);
 		label1.getChildren().addAll(lblCircle, txtCircle);
-
-		Label lblTriangle = new Label("Triangle Speed:");
-		TextField txtTriangle = new TextField("10");
-		HBox label2 = new HBox(10);
-		label2.setAlignment(Pos.CENTER);
-		label2.getChildren().addAll(lblTriangle, txtTriangle);
-
 		Label lblSquare = new Label("Square Speed:");
 		TextField txtSquare = new TextField("10");
+		HBox label2 = new HBox(10);
+		label2.setAlignment(Pos.CENTER);
+		label2.getChildren().addAll(lblSquare, txtSquare);
+		Label lblTriangle = new Label("Triangle Speed:");
+		TextField txtTriangle = new TextField("10");
 		HBox label3 = new HBox(10);
 		label3.setAlignment(Pos.CENTER);
-		label3.getChildren().addAll(lblSquare, txtSquare);
-
+		label3.getChildren().addAll(lblTriangle, txtTriangle);
 		Label lblRhombus = new Label("Rhombus Speed:");
 		TextField txtRhombus = new TextField("10");
 		HBox label4 = new HBox(10);
 		label4.setAlignment(Pos.CENTER);
 		label4.getChildren().addAll(lblRhombus, txtRhombus);
-
 		// Start button
 		Button btnStart = new Button("Start Simulation");
-		btnStart.setOnAction(new startButtonListener(stage, new TextField [] {txtCircle, txtTriangle, txtSquare, txtRhombus}));
+		btnStart.setOnAction(new Simulation(new TextField [] {txtCircle, txtSquare, txtTriangle, txtRhombus}));
 		HBox button = new HBox(10);
 		button.setAlignment(Pos.CENTER);
 		button.getChildren().addAll(btnStart);
-
 		// Displays the GUI
 		VBox vbox = new VBox(40);
 		vbox.setPadding(new Insets(30, 30, 30, 30));
-		vbox.getChildren().addAll(label1, label2, label3, label4, button);
-
-		Scene scene = new Scene(vbox, 450, 375);
+		vbox.getChildren().addAll(label0, label1, label2, label3, label4, button);
+		Scene scene = new Scene(vbox, 450, 425);
 		stage.setScene(scene);
 		stage.show();
-	}
-
-	/*
-	* Called on start button press
-	*/
-	private class startButtonListener implements EventHandler <ActionEvent> 
-	{
-		private TextField [] textFields;
-		private Stage stage;
-
-		/*
-		* Class constructor
-		*/
-		private startButtonListener(Stage stage, TextField [] textFields)
-		{
-			this.textFields = textFields;
-			this.stage = stage;
-		}
-
-		/*
-		* Collects speeds of the objects
-		*/
-		@Override
-		public void handle(ActionEvent event) 
-		{
-			double [] objectSpeeds = new double [4];
-
-			for (int index = 0; index < this.textFields.length; index ++)
-			{
-				try 
-				{
-					objectSpeeds[index] = Double.parseDouble(this.textFields[index].getText());
-
-					// If speed is negative
-					if (objectSpeeds[index] < 0)
-					{
-						objectSpeeds[index] = 1;
-					}
-				}
-				catch (NumberFormatException exception)
-				{
-					objectSpeeds[index] = 10;
-				}
-			}
-
-			startSimulation(objectSpeeds);
-		}
-
-		/* Begins the simulation */
-		private void startSimulation(double [] objectSpeeds) {
-			// Creates the GUI window
-			Stage animationStage = new Stage();
-			animationStage.initModality(Modality.APPLICATION_MODAL);
-			animationStage.setTitle("Mutual Exclusion Simulation");
-			animationStage.setResizable(false);
-
-			// Creates the moving objects (circle, square, triangle, rhombus)
-			Group objects = new Group();
-			Circle circle = new Circle(10, Color.RED);
-			Rectangle square = new Rectangle(15, 15, Color.BLUE);
- 			Polygon triangle = new Polygon();
- 			triangle.setFill(Color.YELLOW);
-        	triangle.getPoints().addAll(new Double[] { 0.0, 0.0, 20.0, 10.0, 10.0, 20.0 });
-        	Polygon rhombus = new Polygon();
-        	rhombus.setFill(Color.GREEN);
-        	rhombus.getPoints().addAll(new Double[] { 0.0, 0.0, -10.0, 10.0, 0.0, 20.0, 10.0, 10.0 });
-			objects.getChildren().add(circle);
-			objects.getChildren().add(square);
-			objects.getChildren().add(triangle);
-			objects.getChildren().add(rhombus);
-
-			// Left triangle path for circle
-			MoveTo cLTMoveTo = new MoveTo();
-			cLTMoveTo.setX(250.0f);
-			cLTMoveTo.setY(250.0f);
-			LineTo cLLowerLine = new LineTo();
-			cLLowerLine.setX(100.0f);
-			cLLowerLine.setY(400.0f);
-			LineTo cLVLine = new LineTo();
-			cLVLine.setX(100.0f);
-			cLVLine.setY(100.0f);
-			LineTo cLUpperLine = new LineTo();
-			cLUpperLine.setX(250.0f);
-			cLUpperLine.setY(250.0f);
-			Path cLTriangle = new Path();
-			cLTriangle.setStroke(Color.BLACK);
-			cLTriangle.getElements().add(cLTMoveTo);
-			cLTriangle.getElements().add(cLLowerLine);
-			cLTriangle.getElements().add(cLVLine);
-			cLTriangle.getElements().add(cLUpperLine);
-			objects.getChildren().add(cLTriangle);
-
-			// Left triangle path for square
-			MoveTo sLTMoveTo = new MoveTo();
-			sLTMoveTo.setX(250.0f);
-			sLTMoveTo.setY(250.0f);
-			LineTo sLUpperLine = new LineTo();
-			sLUpperLine.setX(100.0f);
-			sLUpperLine.setY(100.0f);
-			LineTo sLVLine = new LineTo();
-			sLVLine.setX(100.0f);
-			sLVLine.setY(400.0f);
-			LineTo sLLowerLine = new LineTo();
-			sLLowerLine.setX(250.0f);
-			sLLowerLine.setY(250.0f);
-			Path sLTriangle = new Path();
-			sLTriangle.setStroke(Color.BLACK);
-			sLTriangle.getElements().add(sLTMoveTo);
-			sLTriangle.getElements().add(sLUpperLine);
-			sLTriangle.getElements().add(sLVLine);
-			sLTriangle.getElements().add(sLLowerLine);
-			objects.getChildren().add(sLTriangle);
-
-			// Right triangle path for triangle
-			MoveTo tRTMoveTo = new MoveTo();
-			tRTMoveTo.setX(800.0f);
-			tRTMoveTo.setY(250.0f);
-			LineTo tRUpperLine = new LineTo();
-			tRUpperLine.setX(950.0f);
-			tRUpperLine.setY(100.0f);
-			LineTo tRVLine = new LineTo();
-			tRVLine.setX(950.0f);
-			tRVLine.setY(400.0f);
-			LineTo tRLowerLine = new LineTo();
-			tRLowerLine.setX(800.0f);
-			tRLowerLine.setY(250.0f);
-			Path tRTriangle = new Path();
-			tRTriangle.setStroke(Color.BLACK);
-			tRTriangle.getElements().add(tRTMoveTo);
-			tRTriangle.getElements().add(tRUpperLine);
-			tRTriangle.getElements().add(tRVLine);
-			tRTriangle.getElements().add(tRLowerLine);
-			objects.getChildren().add(tRTriangle);
-
-			// Right triangle path for rhombus
-			MoveTo rRTMoveTo = new MoveTo();
-			rRTMoveTo.setX(800.0f);
-			rRTMoveTo.setY(250.0f);
-			LineTo rRLowerLine = new LineTo();
-			rRLowerLine.setX(950.0f);
-			rRLowerLine.setY(400.0f);
-			LineTo rRVLine = new LineTo();
-			rRVLine.setX(950.0f);
-			rRVLine.setY(100.0f);
-			LineTo rRUpperLine = new LineTo();
-			rRUpperLine.setX(800.0f);
-			rRUpperLine.setY(250.0f);
-			Path rRTriangle = new Path();
-			rRTriangle.setStroke(Color.BLACK);
-			rRTriangle.getElements().add(rRTMoveTo);
-			rRTriangle.getElements().add(rRLowerLine);
-			rRTriangle.getElements().add(rRVLine);
-			rRTriangle.getElements().add(rRUpperLine);
-			objects.getChildren().add(rRTriangle);
-
-			// Displays the path and objects
-			Scene animationScene = new Scene(objects, 1050, 500);
-			animationStage.setScene(animationScene);
-			animationStage.show();
-
-			// Creates message queue objects
-			MessageQueue cQueue = new MessageQueue("circle");
-			MessageQueue tQueue = new MessageQueue("triangle");
-			MessageQueue sQueue = new MessageQueue("square");
-			MessageQueue rQueue = new MessageQueue("rhombus");
-
-			try (PrintWriter writer = new PrintWriter(new FileWriter("../logs/Log.txt"))) { 
-				// Starts the animations
-				PathTransition cLTTrack = new PathTransition(Duration.millis(objectSpeeds[0] * 1000), cLTriangle, circle);
-				cLTTrack.setOnFinished(new EventHandler <ActionEvent> () {
-					@Override
-					public void handle (ActionEvent event) {
-						requestCriticalSection(writer, cQueue, tQueue, sQueue, rQueue);
-					}
-				});
-				cLTTrack.play();
-				PathTransition tRTTrack = new PathTransition(Duration.millis(objectSpeeds[1] * 1000), tRTriangle, triangle);
-				tRTTrack.setOnFinished(new EventHandler <ActionEvent> () {
-					@Override
-					public void handle (ActionEvent event) {
-						requestCriticalSection(writer, tQueue, cQueue, sQueue, rQueue);
-					}
-				});
-				tRTTrack.play();
-				PathTransition sLTTrack = new PathTransition(Duration.millis(objectSpeeds[2] * 1000), sLTriangle, square);
-				sLTTrack.setOnFinished(new EventHandler <ActionEvent> () {
-					@Override
-					public void handle (ActionEvent event) {
-						requestCriticalSection(writer, sQueue, cQueue, tQueue, rQueue);
-					}
-				});
-				sLTTrack.play();
-				PathTransition rRTTrack = new PathTransition(Duration.millis(objectSpeeds[3] * 1000), rRTriangle, rhombus);
-				rRTTrack.setOnFinished(new EventHandler <ActionEvent> () {
-					@Override
-					public void handle (ActionEvent event) {
-						requestCriticalSection(writer, rQueue, cQueue, tQueue, sQueue);
-					}
-				});
-				rRTTrack.play();
-			}
-			catch (IOException exception) {
-				System.out.println("No file found");
-			}
-		}
-
-		/* Requests access to the critical section */
-		public void requestCriticalSection(PrintWriter writer, MessageQueue sender, MessageQueue receiver1, MessageQueue receiver2, MessageQueue receiver3) {
-			sender.sendMessage(writer, sender, new Timestamp(System.currentTimeMillis()), 0);
-			sender.sendMessage(writer, receiver1, new Timestamp(System.currentTimeMillis()), 0);
-			sender.sendMessage(writer, receiver2, new Timestamp(System.currentTimeMillis()), 0);
-			sender.sendMessage(writer, receiver3, new Timestamp(System.currentTimeMillis()), 0);
-		}
 	}
 }
---
+++ src/Simulation.java
@@ -0,0 +1,249 @@
+/*
+MIT License
+
+Copyright (c) 2018 Alex Cadigan, Jacob Naranjo, Tanush Samson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+import javafx.animation.PathTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+/* Begins the simulation */
+public class Simulation implements EventHandler<ActionEvent> {
+	private TextField [] textFields;
+	private double [] objectSpeeds = new double [4];
+	/* Initializes instance variables */
+	public Simulation(TextField [] textFields) {
+		this.textFields = textFields;
+	}
+	/* Collects the speeds entered by the user */
+	@Override
+	public void handle(ActionEvent event) {
+		for (int index = 0; index < this.textFields.length; index ++) {
+			try {
+				this.objectSpeeds[index] = Double.parseDouble(this.textFields[index].getText());
+				if (this.objectSpeeds[index] < 0) {
+					this.objectSpeeds[index] = 1;
+				}
+			}
+			catch (NumberFormatException exception) {
+				objectSpeeds[index] = 10;
+			}
+		}
+		this.buildSimulation();
+	}
+	/* Creates the simulation GUI */
+	private void buildSimulation() {
+		// Creates the GUI window
+		Stage animationStage = new Stage();
+		animationStage.initModality(Modality.APPLICATION_MODAL);
+		animationStage.setTitle("Mutual Exclusion Simulation");
+		animationStage.setResizable(false);
+		// Creates the moving objects (circle, square, triangle, rhombus)
+		Group objects = new Group();
+		Circle circle = new Circle(10, Color.RED);
+		Rectangle square = new Rectangle(15, 15, Color.BLUE);
+		Polygon triangle = new Polygon();
+		triangle.setFill(Color.YELLOW);
+    	triangle.getPoints().addAll(new Double[] { 0.0, 0.0, 20.0, 10.0, 10.0, 20.0 });
+    	Polygon rhombus = new Polygon();
+    	rhombus.setFill(Color.GREEN);
+    	rhombus.getPoints().addAll(new Double[] { 0.0, 0.0, -10.0, 10.0, 0.0, 20.0, 10.0, 10.0 });
+		objects.getChildren().add(circle);
+		objects.getChildren().add(square);
+		objects.getChildren().add(triangle);
+		objects.getChildren().add(rhombus);
+		// Left lower triangle
+		MoveTo lLTMoveTo = new MoveTo();
+		lLTMoveTo.setX(250.0f);
+		lLTMoveTo.setY(250.0f);
+		LineTo lLTLowerLine = new LineTo();
+		lLTLowerLine.setX(100.0f);
+		lLTLowerLine.setY(400.0f);
+		LineTo lLTVLine = new LineTo();
+		lLTVLine.setX(100.0f);
+		lLTVLine.setY(100.0f);
+		LineTo lLTUpperLine = new LineTo();
+		lLTUpperLine.setX(250.0f);
+		lLTUpperLine.setY(250.0f);
+		Path lLTriangle = new Path();
+		lLTriangle.setStroke(Color.BLACK);
+		lLTriangle.getElements().add(lLTMoveTo);
+		lLTriangle.getElements().add(lLTLowerLine);
+		lLTriangle.getElements().add(lLTVLine);
+		lLTriangle.getElements().add(lLTUpperLine);
+		objects.getChildren().add(lLTriangle);
+		// Left upper triangle
+		MoveTo lUTMoveTo = new MoveTo();
+		lUTMoveTo.setX(250.0f);
+		lUTMoveTo.setY(250.0f);
+		LineTo lUTUpperLine = new LineTo();
+		lUTUpperLine.setX(100.0f);
+		lUTUpperLine.setY(100.0f);
+		LineTo lUTVLine = new LineTo();
+		lUTVLine.setX(100.0f);
+		lUTVLine.setY(400.0f);
+		LineTo lUTLowerLine = new LineTo();
+		lUTLowerLine.setX(250.0f);
+		lUTLowerLine.setY(250.0f);
+		Path lUTriangle = new Path();
+		lUTriangle.setStroke(Color.BLACK);
+		lUTriangle.getElements().add(lUTMoveTo);
+		lUTriangle.getElements().add(lUTUpperLine);
+		lUTriangle.getElements().add(lUTVLine);
+		lUTriangle.getElements().add(lUTLowerLine);
+		objects.getChildren().add(lUTriangle);
+		// Right lower triangle
+		MoveTo rLTMoveTo = new MoveTo();
+		rLTMoveTo.setX(800.0f);
+		rLTMoveTo.setY(250.0f);
+		LineTo rLTLowerLine = new LineTo();
+		rLTLowerLine.setX(950.0f);
+		rLTLowerLine.setY(400.0f);
+		LineTo rLTVLine = new LineTo();
+		rLTVLine.setX(950.0f);
+		rLTVLine.setY(100.0f);
+		LineTo rLTUpperLine = new LineTo();
+		rLTUpperLine.setX(800.0f);
+		rLTUpperLine.setY(250.0f);
+		Path rLTriangle = new Path();
+		rLTriangle.setStroke(Color.BLACK);
+		rLTriangle.getElements().add(rLTMoveTo);
+		rLTriangle.getElements().add(rLTLowerLine);
+		rLTriangle.getElements().add(rLTVLine);
+		rLTriangle.getElements().add(rLTUpperLine);
+		objects.getChildren().add(rLTriangle);
+		// Right upper triangle
+		MoveTo rUTMoveTo = new MoveTo();
+		rUTMoveTo.setX(800.0f);
+		rUTMoveTo.setY(250.0f);
+		LineTo rUTUpperLine = new LineTo();
+		rUTUpperLine.setX(950.0f);
+		rUTUpperLine.setY(100.0f);
+		LineTo rUTVLine = new LineTo();
+		rUTVLine.setX(950.0f);
+		rUTVLine.setY(400.0f);
+		LineTo rUTLowerLine = new LineTo();
+		rUTLowerLine.setX(800.0f);
+		rUTLowerLine.setY(250.0f);
+		Path rUTriangle = new Path();
+		rUTriangle.setStroke(Color.BLACK);
+		rUTriangle.getElements().add(rUTMoveTo);
+		rUTriangle.getElements().add(rUTUpperLine);
+		rUTriangle.getElements().add(rUTVLine);
+		rUTriangle.getElements().add(rUTLowerLine);
+		objects.getChildren().add(rUTriangle);
+		// Left critical section
+		MoveTo lCSMoveTo = new MoveTo();
+		lCSMoveTo.setX(250.0f);
+		lCSMoveTo.setY(250.0f);
+		LineTo lCSLine = new LineTo();
+		lCSLine.setX(800.0f);
+		lCSLine.setY(250.0f);
+		Path lCSPath = new Path();
+		lCSPath.setStroke(Color.RED);
+		lCSPath.getElements().add(lCSMoveTo);
+		lCSPath.getElements().add(lCSLine);
+		objects.getChildren().add(lCSPath);
+		// Right critical section
+		MoveTo rCSMoveTo = new MoveTo();
+		rCSMoveTo.setX(800.0f);
+		rCSMoveTo.setY(250.0f);
+		LineTo rCSLine = new LineTo();
+		rCSLine.setX(250.0f);
+		rCSLine.setY(250.0f);
+		Path rCSPath = new Path();
+		rCSPath.setStroke(Color.RED);
+		rCSPath.getElements().add(rCSMoveTo);
+		rCSPath.getElements().add(rCSLine);
+		objects.getChildren().add(rCSPath);
+		// Displays the path and objects
+		Scene animationScene = new Scene(objects, 1050, 500);
+		animationStage.setScene(animationScene);
+		animationStage.show();	
+		// Begins running the simulation
+		this.runSimulation(this.objectSpeeds, circle, square, triangle, rhombus, lLTriangle, lUTriangle, rLTriangle, rUTriangle, lCSPath, rCSPath);
+	}
+	/* Runs the simulation */
+	private void runSimulation(double [] objectSpeeds, Circle circle, Rectangle square, Polygon triangle, Polygon rhombus, Path lLTriangle, Path lUTriangle, Path rLTriangle, Path rUTriangle, Path lCSPath, Path rCSPath) {
+		PathTransition cLTTrack = new PathTransition(Duration.millis(objectSpeeds[0] * 1000), lLTriangle, circle);
+		cLTTrack.setOnFinished(new EventHandler <ActionEvent> () {
+			@Override
+			public void handle (ActionEvent event) {
+				PathTransition cCSLTrack = new PathTransition(Duration.millis(objectSpeeds[0] * 1000), lCSPath, circle);
+				cCSLTrack.setOnFinished(new EventHandler <ActionEvent> () {
+					@Override
+					public void handle (ActionEvent event) {
+						PathTransition cRTTrack = new PathTransition(Duration.millis(objectSpeeds[0] * 1000), rUTriangle, circle);
+						cRTTrack.setOnFinished(new EventHandler <ActionEvent> () {
+							@Override
+							public void handle (ActionEvent event) {
+								PathTransition cCSRTrack = new PathTransition(Duration.millis(objectSpeeds[0] * 1000), rCSPath, circle);
+								cCSRTrack.play();
+							}
+						});
+						cRTTrack.play();
+					}
+				});
+				cCSLTrack.play();
+			}
+		});
+		cLTTrack.play();
+		PathTransition sLTTrack = new PathTransition(Duration.millis(objectSpeeds[1] * 1000), lUTriangle, square);
+		sLTTrack.setOnFinished(new EventHandler <ActionEvent> () {
+			@Override
+			public void handle (ActionEvent event) {
+				PathTransition sCSLTrack = new PathTransition(Duration.millis(objectSpeeds[1] * 1000), lCSPath, square);
+				sCSLTrack.setOnFinished(new EventHandler <ActionEvent> () {
+					@Override
+					public void handle (ActionEvent event) {
+						PathTransition sRTTrack = new PathTransition(Duration.millis(objectSpeeds[1] * 1000), rLTriangle, square);
+						sRTTrack.setOnFinished(new EventHandler <ActionEvent> () {
+							@Override
+							public void handle (ActionEvent event) {
+								PathTransition sCSRTrack = new PathTransition(Duration.millis(objectSpeeds[1] * 1000), rCSPath, square);
+								sCSRTrack.play();
+							}
+						});
+						sRTTrack.play();
+					}
+				});
+				sCSLTrack.play();
+			}
+		});
+		sLTTrack.play();
+		// Creates the objects for message passing
+		// Begins the simulations
+		// Check if objects want to enter critical section
+			// 
+	}
+}
